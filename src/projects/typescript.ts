fdb1d974aa99bd927da446247b4a812808e1ffbc
iliapolo <epolon@amazon.com>
2023-02-16 13:07:42 +0200

    node project
    Signed-off-by: iliapolo <epolon@amazon.com>

--- src/projects/typescript.ts
+++ src/projects/typescript.ts
@@ -1,51 +1,10 @@
 import { typescript } from 'projen';
-
-export const NAME_PREFIX = 'cdk8s-';
-export const SCOPE = '@cdk8s/';
+import * as node from './node';
 
 /**
- * Subset of options that should be fixed for all cdk8s-team typescript projects.
- * These will not be available for customization by individual projects.
+ * Options for `Cdk8sTeamTypeScriptProject`.
  */
-export const fixedOptionsKeys = [
-  'authorName',
-  'authorEmail',
-  'repository',
-  'releaseToNpm',
-  'autoApproveOptions',
-  'autoApproveUpgrades',
-  'minNodeVersion',
-] as const;
-export type fixedOptionsKeysType = typeof fixedOptionsKeys[number];
-
-/**
- * Create the fixed typescript project options.
- */
-export function buildTypeScriptProjectFixedOptions(): Pick<typescript.TypeScriptProjectOptions, fixedOptionsKeysType> {
-
-  return {
-    authorName: 'Amazon Web Services',
-    releaseToNpm: true,
-    autoApproveOptions: {
-      allowedUsernames: ['cdk8s-automation'],
-      secret: 'GITHUB_TOKEN',
-    },
-    autoApproveUpgrades: true,
-    minNodeVersion: '14.17.0',
-  };
-}
-
-/**
- * Create the repository name based on the project name.
- */
-export function buildRepositoryName(projectName: string) {
-  return projectName.startsWith(SCOPE) ? projectName.replace(SCOPE, NAME_PREFIX) : projectName;
-}
-
-/**
- * Options for `Cdk8sTeamTypescriptProject`.
- */
-export interface Cdk8sTeamTypescriptProjectOptions extends typescript.TypeScriptProjectOptions {
+export interface Cdk8sTeamTypeScriptProjectOptions extends typescript.TypeScriptProjectOptions {
 
   /**
    * The name of the repository inside the cdk8s-team
@@ -62,57 +21,21 @@ export interface Cdk8sTeamTypescriptProjectOptions extends typescript.TypeScript
  */
 export class Cdk8sTeamTypeScriptProject extends typescript.TypeScriptProject {
 
-  constructor(options: Cdk8sTeamTypescriptProjectOptions) {
+  constructor(options: Cdk8sTeamTypeScriptProjectOptions) {
 
-    validateOptions(options);
-    validateProjectName(options.name);
+    node.validateOptions(options);
+    node.validateProjectName(options);
 
-    const fixedOptions = buildTypeScriptProjectFixedOptions();
-    const repoName = options.repoName ?? buildRepositoryName(options.name);
+    const fixedOptions = node.buildNodeProjectFixedOptions(options);
+    const defaultOptions = node.buildNodeProjectDefaultOptions(options);
+
 
     super({
       ...fixedOptions,
-      repository: `https://github.com/cdk8s-team/${repoName}.git`,
+      ...defaultOptions,
       ...options,
     });
 
   }
 
 }
-
-/**
- * Validate that the options map does not contain any invalid option.
- * This would usually be implemented at compile time using Omit/Pick but jsii
- * doesn't allow this.
- */
-export function validateOptions(options: any) {
-
-  const keys = Object.keys(options);
-
-  for (const key of fixedOptionsKeys) {
-    if (keys.includes(key)) {
-      throw new Error(`Invalid option: ${key}`);
-    }
-  }
-
-}
-
-/**
- * Validate the name of a project.
- */
-export function validateProjectName(name: string) {
-
-  const scoped = name.startsWith('@');
-
-  if (scoped) {
-    if (!name.startsWith(SCOPE)) {
-      throw new Error(`Illegal project scope: ${name}. Scope must be '@cdk8s/'`);
-    }
-  } else {
-    if (!name.startsWith(NAME_PREFIX)) {
-      throw new Error(`Illegal project name: ${name}. Name must start with 'cdk8s-'`);
-    }
-  }
-
-}
-
--- test/projects/typescript.test.ts
+++ test/projects/typescript.test.ts
@@ -4,7 +4,7 @@ import * as src from '../../src';
 test('typescript project name must start with cdk8s-', () => {
 
   expect(() => {
-    new src.Cdk8sTeamTypescriptProject({
+    new src.Cdk8sTeamTypeScriptProject({
       name: 'sample',
       defaultReleaseBranch: 'main',
     });
@@ -14,7 +14,7 @@ test('typescript project name must start with cdk8s-', () => {
 
 test('default typescript project', () => {
 
-  const project = new src.Cdk8sTeamTypescriptProject({
+  const project = new src.Cdk8sTeamTypeScriptProject({
     name: 'cdk8s-sample',
     defaultReleaseBranch: 'main',
   });
@@ -25,7 +25,7 @@ test('default typescript project', () => {
 
 test('scoped default typescript project', () => {
 
-  const project = new src.Cdk8sTeamTypescriptProject({
+  const project = new src.Cdk8sTeamTypeScriptProject({
     name: '@cdk8s/sample',
     defaultReleaseBranch: 'main',
   });
@@ -37,11 +37,35 @@ test('scoped default typescript project', () => {
 test('throws on invalid option', () => {
 
   expect(() => {
-    new src.Cdk8sTeamTypescriptProject({
+    new src.Cdk8sTeamTypeScriptProject({
       name: 'sample',
       defaultReleaseBranch: 'main',
       authorName: 'foor',
     });
   }).toThrowError('Invalid option: authorName');
+
+});
+
+test('allow not releasing to npm', () => {
+
+  const project = new src.Cdk8sTeamTypeScriptProject({
+    name: 'root',
+    defaultReleaseBranch: 'main',
+    releaseToNpm: false,
+  });
+
+  expect(Testing.synth(project)).toMatchSnapshot();
+
+});
+
+test('allow not releasing at all', () => {
+
+  const project = new src.Cdk8sTeamTypeScriptProject({
+    name: 'root',
+    defaultReleaseBranch: 'main',
+    release: false,
+  });
+
+  expect(Testing.synth(project)).toMatchSnapshot();
 
 });
