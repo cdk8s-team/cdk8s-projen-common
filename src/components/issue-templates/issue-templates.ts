50c44725265d7deee7f0824b678a5ea3052ed3cf
Eli Polonsky <Eli.polonsky@gmail.com>
2024-09-16 11:42:44 +0300

    chore: centralize issues in `cdk8s` repository (#961)
    A while back we had a [discussion](https://github.com/cdk8s-team/cdk8s/discussions/1274) on whether to have issues only in the `cdk8s` repository with appropriate labels, as opposed to scattered across 3 repos. Quick poll on slack showed the community is in favor, and I have no objections - so lets do it.

--- src/components/issue-templates/issue-templates.ts
+++ src/components/issue-templates/issue-templates.ts
@@ -24,32 +24,44 @@ export class IssueTemplates extends Component {
 
     const repoName = options.repoName;
 
+    const contactLinks = [
+      {
+        name: 'Slack Community Support @ cdk.dev',
+        url: 'https://cdk-dev.slack.com/archives/C0184GCBY4X',
+        about: 'Please ask and answer questions here.',
+      },
+      {
+        name: 'Slack Community Support @ cncf.io',
+        url: 'https://cloud-native.slack.com/archives/C02KCDACGTT',
+        about: 'Please ask and answer questions here.',
+      },
+      {
+        name: 'GitHub Community Support',
+        url: `https://github.com/cdk8s-team/${repoName}/discussions`,
+        about: 'Please ask and answer questions here.',
+      },
+    ];
+
+    if (repoName === 'cdk8s') {
+      this.addFile('bug.md');
+      this.addFile('feature-request.md');
+    } else {
+      contactLinks.push(
+        {
+          name: 'Open a cdk8s issue',
+          url: 'https://github.com/cdk8s-team/cdk8s/issues/new/choose',
+          about: 'All cdk8s issues are tracked in the cdk8s repository.',
+        },
+      );
+    }
+
     // see https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/configuring-issue-templates-for-your-repository#configuring-the-template-chooser
     new YamlFile(this.project, '.github/ISSUE_TEMPLATE/config.yml', {
       obj: {
         blank_issues_enabled: false,
-        contact_links: [
-          {
-            name: 'Slack Community Support @ cdk.dev',
-            url: 'https://cdk-dev.slack.com/archives/C0184GCBY4X',
-            about: 'Please ask and answer questions here.',
-          },
-          {
-            name: 'Slack Community Support @ cncf.io',
-            url: 'https://cloud-native.slack.com/archives/C02KCDACGTT',
-            about: 'Please ask and answer questions here.',
-          },
-          {
-            name: 'GitHub Community Support',
-            url: `https://github.com/cdk8s-team/${repoName}/discussions`,
-            about: 'Please ask and answer questions here.',
-          },
-        ],
+        contact_links: contactLinks,
       },
     });
-
-    this.addFile('bug.md');
-    this.addFile('feature-request.md');
 
   }
 
