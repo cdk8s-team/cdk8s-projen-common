b3de581919db9a4c63e6015dbb34aa24210ed360
epolon <epolon@amazon.com>
2024-09-16 09:11:42 +0300

    mid work
    Signed-off-by: epolon <epolon@amazon.com>

--- src/components/issue-templates/issue-templates.ts
+++ src/components/issue-templates/issue-templates.ts
@@ -24,6 +24,37 @@ export class IssueTemplates extends Component {
 
     const repoName = options.repoName;
 
+    const contactLinks = [
+      {
+        name: 'Slack Community Support @ cdk.dev',
+        url: 'https://cdk-dev.slack.com/archives/C0184GCBY4X',
+        about: 'Please ask and answer questions here.',
+      },
+      {
+        name: 'Slack Community Support @ cncf.io',
+        url: 'https://cloud-native.slack.com/archives/C02KCDACGTT',
+        about: 'Please ask and answer questions here.',
+      },
+      {
+        name: 'GitHub Community Support',
+        url: `https://github.com/cdk8s-team/${repoName}/discussions`,
+        about: 'Please ask and answer questions here.',
+      },
+    ];
+
+    if (repoName === 'cdk8s') {
+      this.addFile('bug.md');
+      this.addFile('feature-request.md');
+    } else {
+      contactLinks.push(
+        {
+          name: 'Open a cdk8s issue',
+          url: 'https://github.com/cdk8s-team/cdk8s/issues/new/choose',
+          about: 'All cdk8s issues are tracked in the cdk8s repository. Follow the link to open an issue on an subject.',
+        },
+      );
+    }
+
     // see https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/configuring-issue-templates-for-your-repository#configuring-the-template-chooser
     new YamlFile(this.project, '.github/ISSUE_TEMPLATE/config.yml', {
       obj: {
@@ -47,9 +78,6 @@ export class IssueTemplates extends Component {
         ],
       },
     });
-
-    this.addFile('bug.md');
-    this.addFile('feature-request.md');
 
   }
 
